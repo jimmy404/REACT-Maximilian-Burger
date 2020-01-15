cbf28358f69bd0432322ef751007fb467fbf51a3
Jimmy <bruno_pier@hotmail.com>
2020-01-14 23:42:26 -0300

    adding code class -Order Submission & Passing Data Between Pages- BurgerBuilder.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,32 +94,13 @@ class BurgerBuilder extends Component {
     }
     purchaseContinueHandler = () => {
         //alert('You continue!');
-        //this.setState({loading: true});
-        //const order = {
-        //    ingredients: this.state.ingredients,
-        //    price: this.state.totalPrice,
-        //    customer: {
-        //        name: 'Jimmy',
-        //        adress: {
-        //            street: 'Testsreet 1',
-        //            zipCode: '5000',
-        //            country: 'Argentina'
-        //        },
-        //        email: 'test@test.com'
-        //    },
-        //    deliveryMethod: 'fastest'
-        //}
-        //axios.post('/orders.json', order)
-        //.then(response => {
-        //    this.setState({loading: false, purchasing: false});
-        //})
-        //.catch(error => {
-        //    this.setState({loading: false, purchasing: false});
-        //});
         const queryParams = [];
         for(let i in this.state.ingredient){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+
+        queryParams.push('price=' + this.state.totalPrice);
+
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
